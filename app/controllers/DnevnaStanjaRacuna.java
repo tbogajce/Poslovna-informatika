714e1bb5f73058630623cc5ed3afe6e2c27e03d9
Unknown <igx33@live.com>
2017-05-28 18:44:40 +0200

    Zapoceo rad na Dnevnom Stanju racuna.. (nisam zavrsio)

---
+++ app/controllers/DnevnaStanjaRacuna.java
@@ -0,0 +1,83 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.DnevnoStanjeRacuna;
+import models.Drzava;
+import models.NaseljenoMesto;
+import models.Racun;
+import play.mvc.Controller;
+
+public class DnevnaStanjaRacuna extends Controller{
+
+	
+	public static void show(String mode, Long selectedId)
+	{
+		List<Racun> racuni = Racun.findAll();
+		List<DnevnoStanjeRacuna> dnevnaStanjaRacuna = DnevnoStanjeRacuna.findAll();
+		if(mode == null || mode.equals(""))
+			mode = "edit";
+		render(racuni,dnevnaStanjaRacuna,mode,selectedId);
+	}
+	
+	public static void nextMehanizam(Long id)
+	{
+		System.out.println(id);
+		
+		Racun rac = Racun.findById(id);
+		List<DnevnoStanjeRacuna> dnevnaStanjaRacuna = DnevnoStanjeRacuna.findAll();
+		List<DnevnoStanjeRacuna> dnevnaStanjaZaPrikaz = new ArrayList<DnevnoStanjeRacuna>();
+		List<Racun> racuni = Racun.findAll();
+		
+		for(DnevnoStanjeRacuna nm : dnevnaStanjaRacuna)
+		{
+			if(nm.racun.id == rac.id)
+			{
+				dnevnaStanjaZaPrikaz.add(nm);
+				System.out.println("naslo dnevno stanje racuna..");
+			}
+		}
+		String mode = "edit";
+		dnevnaStanjaRacuna.clear();
+		dnevnaStanjaRacuna.addAll(dnevnaStanjaZaPrikaz);
+		//render(drzave,mestaZaPrikaz,"edit",0);
+		Long idZaPrikaz = id;
+		renderTemplate("DnevnaStanjaRacuna/show.html",racuni,dnevnaStanjaRacuna,mode,0,idZaPrikaz);
+		
+	}
+	
+	public static void create(DnevnoStanjeRacuna dnevnoStanjeRacuna, Long racun)
+	{
+		System.out.println("EDIT: "+dnevnoStanjeRacuna.id);
+		Racun racunx = Racun.findById(racun);
+		dnevnoStanjeRacuna.racun=racunx;
+		dnevnoStanjeRacuna.save();
+		show("add",dnevnoStanjeRacuna.id);
+	}
+	
+	public static void edit(DnevnoStanjeRacuna dnevnoStanjeRacuna, Long racun)
+	{
+		System.out.println("EDIT: "+dnevnoStanjeRacuna.id);
+		Racun racunx = Racun.findById(racun);
+		dnevnoStanjeRacuna.racun=racunx;
+		dnevnoStanjeRacuna.save();
+		show("edit",dnevnoStanjeRacuna.id);
+		
+	}
+	
+	public static void filter(DnevnoStanjeRacuna dnevnoStanjeRacuna)
+	{
+		List<DnevnoStanjeRacuna> dnevnaStanjaRacuna = DnevnoStanjeRacuna.find("byDatumLikeAndRacunLike", "%"+dnevnoStanjeRacuna.datum+"%", "%"+dnevnoStanjeRacuna.racun+"%").fetch();
+		String mode = "edit";
+		renderTemplate("DnevnaStanjaRacuna/show.html", dnevnaStanjaRacuna, mode );
+	}
+	
+	public static void delete(Long id)
+	{
+		DnevnoStanjeRacuna dnevnoStanjeRacuna = DnevnoStanjeRacuna.findById(id);
+		dnevnoStanjeRacuna.delete();
+		show("edit",dnevnoStanjeRacuna.id-1);
+	}
+	
+}
