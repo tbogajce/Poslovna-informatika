1e4c65a9dd5d2398e980d01dee0f0cb3cd3222c8
skilja95 <milivojeskiljevic7@gmail.com>
2017-05-29 23:36:26 +0200

    CRUD za banku
    Odradjene osnovne operacije sa bankom

---
+++ app/controllers/Banke.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import java.util.List;
+
+import models.Banka;
+import models.Drzava;
+import play.mvc.Controller;
+
+public class Banke extends Controller{
+	
+	public static void show(String mode, Long selectedIndex)
+	{
+		List<Banka> banke = Banka.findAll();
+		if(mode == null || mode.equals(""))
+			mode = "edit";
+		render(banke,mode,selectedIndex);
+	}
+	
+	
+	
+	public static void create(Banka banka )
+	{
+		System.out.println("CREATE: "+banka.nazivBanke+", "+banka.id);
+		banka.save();
+		show("add",banka.id);
+	}
+	
+	public static void edit(Banka banka)
+	{
+		System.out.println("EDIT: "+banka.nazivBanke+", "+banka.id);
+		banka.save();
+		show("edit",banka.id);
+		
+	}
+	
+	public static void filter(Banka banka)
+	{
+		List<Banka> banke = Banka.find("bySifraBankeLikeAndNazivBankeLike", "%"+banka.sifraBanke+"%", "%"+banka.nazivBanke+"%").fetch();
+		String mode = "edit";
+		renderTemplate("Banke/show.html", banke, mode );
+	}
+	
+	public static void delete(Long id)
+	{
+		Banka ban = Banka.findById(id);
+		ban.delete();
+		show("edit", ban.id-1);
+	}
+
+}
