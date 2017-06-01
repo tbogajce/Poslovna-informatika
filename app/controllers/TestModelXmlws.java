514c5740fa4ef0d39ec0277afe2a2b87618e3a66
skilja95 <milivojeskiljevic7@gmail.com>
2017-06-01 15:45:04 +0200

    Dodate stvari za JAXB
    Napravljen probni xml za naloge, kao i sema, bez neke prevelike validacije nad semom. Izgenerisane klase i napravljen probni kontroler za pristupanje podacima, unmarshalling

---
+++ app/controllers/TestModelXmlws.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import model.xmlws.Nalozi;
+
+public class TestModelXmlws {
+	public void test() {
+		try {
+			
+			System.out.println("[INFO] Example 5: JAXB unmarshalling.\n");
+			
+			// Definiše se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
+			JAXBContext context = JAXBContext.newInstance("model.xmlws");
+			
+			// Unmarshaller je objekat zadužen za konverziju iz XML-a u objektni model
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			
+			// Unmarshalling generiše objektni model na osnovu XML fajla 
+			Nalozi nalozi = (Nalozi) unmarshaller.unmarshal(new File("primjerXml.xml"));
+
+			// Prikazuje unmarshallovan objekat
+			printNalozi(nalozi);
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void printNalozi(Nalozi nalozi) {
+		
+		// Prikaz svih odseka
+		for(Nalozi.NalogZaPrenos nalogZaPrenos : nalozi.getNalogZaPrenos())
+			System.out.println(nalogZaPrenos.getPodaciODuzniku().getIme());
+		
+	}
+	
+	
+    public static void main( String[] args ) {
+    	TestModelXmlws test = new TestModelXmlws();
+    	test.test();
+    }
+}
